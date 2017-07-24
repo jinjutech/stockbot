be7044c882bf0e990511663bf737529c2fb8601d
班玉都 <banyudu@icloud.com>
2017-07-24 08:24:38 +0800

    add sendToDingding function

--- stock/handler.js
+++ stock/handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const request = require('request');
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -10,6 +12,38 @@ module.exports.hello = (event, context, callback) => {
   };
 
   callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.sendToDingding = (event, context, callback) => {
+  const msg = 'hello';
+  const accessToken = '601d8c7b44ca18e1472f42c24b8eb791074071b2c0ee305af0a4f096996dad0b';
+  const url = `https://oapi.dingtalk.com/robot/send?access_token=${accessToken}`;
+  const body = {
+    msgtype: 'text', 
+    text: {
+      content: msg,
+    }, 
+    at: {
+      atMobiles: [
+        '156xxxx8827', 
+        '189xxxx8325'
+      ], 
+      isAtAll: false
+    }
+  };
+
+  const options = {
+    uri: url,
+    method: 'POST',
+    json: body,
+  }; 
+
+  request(options, (error, response, body) => {
+    callback(error, response, body);
+  })
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });

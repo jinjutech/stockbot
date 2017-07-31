f639079f6cb2fc4a6a18c838b2bc4676f474e9a2
Yudu Ban <banyudu@gmail.com>
2017-08-01 01:13:52 +0800

    require sina-stock

--- stock/handler.js
+++ stock/handler.js
@@ -1,6 +1,16 @@
 'use strict';
 
 const request = require('request');
+const sinaStock = require('sina-stock');
+
+function getMarkdownMsg(data) {
+  console.log(data)
+  const result = []
+  for (let i = 0; i < data.length; i++) {
+    result.push(`${i + 1}. ${data[i].code}`)
+  }
+  return result.join('\n');
+}
 
 module.exports.hello = (event, context, callback) => {
   const response = {
@@ -18,11 +28,11 @@ module.exports.notifyDingding = (event, context, callback) => {
   const msg = 'hello';
   const accessToken = '601d8c7b44ca18e1472f42c24b8eb791074071b2c0ee305af0a4f096996dad0b';
   const url = `https://oapi.dingtalk.com/robot/send?access_token=${accessToken}`;
+  const stockCodes = ['sz002410', 'sz000333', 'sh601318', 'sh601628', 'sz002632'];
   const body = {
-    msgtype: 'text', 
-    text: {
-      content: msg,
-    }, 
+    msgtype: 'markdown', 
+    title: 'stock',
+    text: '',
     at: {
       atMobiles: [
         '156xxxx8827', 
@@ -38,8 +48,11 @@ module.exports.notifyDingding = (event, context, callback) => {
     json: body,
   }; 
 
-  request(options, (error, response, body) => {
-    callback(error, response, body);
+  sinaStock.stock(stockCodes, (data) => {
+    body.text = getMarkdownMsg(data);
+    request(options, (error, response, body) => {
+      callback(error, response, body);
+    })
   })
 };
 
